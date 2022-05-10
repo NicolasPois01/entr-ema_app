e4ed9a28121519c5ec59ecda5149015a21d7807d
TomRBD <tom.muet@orange.fr>
2022-05-10 13:40:45 +0200

    commit last changes

--- src/app/services/product/product.service.spec.ts
+++ src/app/services/product/product.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ProduitService } from './product.service';
+import { ProductService } from './product.service';
 
 describe('ProduitService', () => {
-  let service: ProduitService;
+  let service: ProductService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(ProduitService);
+    service = TestBed.inject(ProductService);
   });
 
   it('should be created', () => {
